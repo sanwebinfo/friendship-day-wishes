eed2df30075c2353dc57caa5a256009be0e475eb
mskian <10300271+mskian@users.noreply.github.com>
2024-07-08 22:59:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# Friendship Day Wishes
+
+A simple Go application that displays Happy Friendship Day ASCII art and a text greeting with the recipient's name and a social media sharing URL.  
+
+> Just My Fun Project 🙂 and Learning Golang - Happy Friendship Day Wishes 2024  
+
+```yaml
+
+wishes@sanweb:~💚$
+
+
+♥♥
+♥─▀██▀▀▀█
+♥──██▄█
+♥──██▀█
+♥─▄██ ANTASTIC Friend ♥♥
+
+
+
+Friendship is the compass
+that guides us through life's storm
+
+```
+
+## Features
+
+- Friendship Day ASCII art and Text Greeting with name
+- Shareable URL for social media sharing
+- Supports both HTML and plain text responses
+- Proper Error handling and Validations
+
+## Setup
+
+- Clone or Download the Repo
+
+```sh
+
+git clone https://github.com/sanwebinfo/friendship-day-wishes.git
+cd friendship-day-wishes
+
+```
+
+- Start the Server
+
+```sh
+go run wish.go
+```
+
+## Usage
+
+- Send a GET request to the `/wish` endpoint with a name query parameter:
+
+```sh
+http://localhost:6054/wish?name=YourName
+```
+
+- cURL Request
+
+```sh
+
+curl "http://localhost:6054/wish?name=John-Doe"
+
+or
+
+curl -G -d "name=John Doe" http://localhost:6054/wish
+
+```
+
+- httpie
+
+```sh
+http -b GET "http://localhost:6054/wish" "name==John Doe"
+```
+
+## HTML Response
+
+If the Accept header includes `text/html`, you will get a formatted HTML response.
+
+## Plain Text Response
+
+If the Accept header includes `text/plain`, you will get a plain text response with ANSI color codes.
+
+## Build Package
+
+- Run Make file to build a package for your Systems
+
+```sh
+make build
+```
+
+## Packges Build for  
+
+Linux, Apple, Windows and Android - `/makefile`  
+
+- Linux-386
+- Linux-arm-7
+- Linux-amd64
+- Linux-arm64
+- Andriod-arm64
+- windows-386
+- windows-amd64
+- darwin-amd64
+- darwin-arm64
+
+```sh
+chmod +x wish
+./wish
+```
+
+## LICENSE
+
+MIT
---
+++ wish.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+const port = 6054
+
+func escapeText(text string) string {
+	return html.EscapeString(text)
+}
+
+func cleanName(name string) string {
+	return strings.ReplaceAll(name, "-", " ")
+}
+
+func asciiArt(name string) string {
+	cleanedName := cleanName(name)
+	art := `
+
+♥♥
+♥─▀██▀▀▀█
+♥──██▄█
+♥──██▀█
+♥─▄██ ANTASTIC Friend ♥♥
+
+	`
+
+	quotes := []string{
+		"Friendship is the compass\nthat guides us through life's storm",
+		"Friendship doubles your joy\nand divides your sorrow",
+	}
+
+	quote := quotes[len(name)%len(quotes)]
+
+	return fmt.Sprintf("\nwishes@%s:~💚$\n%s\n\n%s\n\n", escapeText(cleanedName), art, quote)
+}
+
+func generateSlug(name string) string {
+	var result []string
+	for _, r := range name {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			result = append(result, string(unicode.ToLower(r)))
+		} else if r == ' ' {
+			result = append(result, "-")
+		}
+	}
+	return strings.Join(result, "")
+}
+
+func validateName(name string) (string, error) {
+	if len(name) == 0 || len(name) > 40 {
+		return "", fmt.Errorf("name length must be between 1 and 40 characters")
+	}
+
+	if valid := regexp.MustCompile(`^[\p{L}\p{N}\p{P}\p{Zs}\p{M}\p{Sm}\p{So}\p{Sk}]+$`).MatchString(name); !valid {
+		return "", fmt.Errorf("name contains invalid characters")
+	}
+
+	return name, nil
+}
+
+func wishHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+
+	validName, err := validateName(name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	name = escapeText(validName)
+	asciiText := asciiArt(name)
+	slugText := generateSlug(name)
+	baseURL := fmt.Sprintf("https://%s", r.Host)
+	shareURL := fmt.Sprintf("%s/wish?name=%s", baseURL, slugText)
+
+	acceptHeader := r.Header.Get("Accept")
+	if strings.Contains(acceptHeader, "text/html") {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+		fmt.Fprintf(w, `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Happy Friendship Wishes</title>
+	<meta name="description" content="Happy Friendship Day ASCII Art Greeting - Friendship Day Greeting Generator."/>
+	<link rel="preconnect" href="https://cdnjs.cloudflare.com">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css" integrity="sha512-IgmDkwzs96t4SrChW29No3NXBIBv8baW490zk5aXvhCD8vuZM3yUSkbyTBcXohkySecyzIrUwiF/qV0cuPcL3Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+	    html, body {
+          min-height: 100vh;
+        }
+        body {
+            font-family: monospace;
+            background-color: #f0f0f0;
+			min-height: 100vh;
+        }
+		#quote-container {
+           margin: 10px auto;
+           border-radius: 10px;
+           padding: 20px;
+           background-color: #222f3e;
+       }
+       #quote {
+           font-size: 20px;
+           margin-bottom: 20px;
+           color: #333;
+        }
+		quote-card {
+		  margin: 20px auto;
+        }
+        pre {
+            font-family: monospace;
+            font-size: 14px;
+            background-color: #4b4b4b;
+            color: #fdcb6e;
+			text-shadow: 0 0 3px #FFC312;
+            padding: 20px;
+            border-radius: 5px;
+			word-wrap: break-word;
+			overflow-x: auto;
+            line-height: inherit;
+        }
+    </style>
+</head>
+<body>
+<section class="section"><div class="container"><div id="quote-card" class="card content"><div id="quote-container">
+   <pre>
+    %s
+	</pre>
+	<br>
+	<pre>curl %s<br><br>URL: %s</pre>
+	<br>
+</div></div></div></section>
+</body>
+</html>
+`, asciiText, shareURL, shareURL)
+	} else {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+		w.Header().Set("X-Robots-Tag", "noindex, nofollow")
+
+		response := fmt.Sprintf("\033[32m%s\033[0m\nShare this on social media: %s\n\n", asciiText, shareURL)
+		fmt.Fprint(w, response)
+	}
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "404 Page Not Found")
+}
+
+func internalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprint(w, "500 Internal Server Error")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/wish", wishHandler)
+	mux.HandleFunc("/404", notFoundHandler)
+	mux.HandleFunc("/500", internalServerErrorHandler)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFoundHandler().ServeHTTP(w, r)
+	})
+
+	log.Printf("Server starting on port %d\n", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
